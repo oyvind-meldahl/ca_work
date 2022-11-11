d8f735d88f03f978d3d7e6bf9ab3ca0aa126e0af
oyvind-meldahl <79719017+oyvind-meldahl@users.noreply.github.com>
2022-11-11 20:21:23 +0100

    home

--- cypress/e2e/logout.testing.cy.js
+++ cypress/e2e/logout.testing.cy.js
@@ -1,6 +1,6 @@
 describe("The logout button logs the user out when clicked.", () => {
   it("Can log out with logout button.", () => {
-    cy.visit("http://localhost:8485");
+    cy.visit("http://127.0.0.1:8485");
     cy.wait(1000);
     cy.get("#registerForm button ").contains("Login").click({ force: true });
 
